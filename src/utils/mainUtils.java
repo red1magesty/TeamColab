c355da3ab456d9e45daecd684e9a17c7c0d35d8e
red1magesty <ilyassi3@hotmail.com>
2023-09-17 18:27:38 -0400

    project set up updated

--- src/utils/mainUtils.java
+++ src/utils/mainUtils.java
@@ -1,8 +1,9 @@
 package utils;
 
 public class mainUtils {
-// 1st line from git to github
-  // 2nd line from github to git
+// 1st line from git to gitHub
+  // 2nd line from gitHub to git
+    // 3rd line from git to gitHub
 
 
 
